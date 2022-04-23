e64634bf1ee0520afdb9b58598fc3a02389d87df
AnthonyMeyers <thony.meyers@gmail.com>
2022-04-23 15:11:57 +0200

    updated team page

--- src/modules/App-team.jsx
+++ src/modules/App-team.jsx
@@ -1,3 +1,41 @@
 export default function Appteam() {
-  return <p>team</p>;
+  return (
+    <>
+      <section class="team">
+        <h2 className="team__title">Team</h2>
+        <article class="team__article">
+          <div class="team__article__imgholder">
+            <img
+              class="team__article__imgholder__img"
+              src="./src/images/gyssens.jpg"
+              alt="Stijn Gyssens"
+            />
+            <span class="team__article__imgholder__name">Stijn Gyssens</span>
+          </div>
+          <div class="team__article__description">
+            <h3 class="team__article__description__job">
+              Back end development
+            </h3>
+            <p class="team__article__description__text">testing</p>
+          </div>
+        </article>
+        <article class="team__article">
+          <div class="team__article__imgholder">
+            <img
+              class="team__article__imgholder__img"
+              alt="Anthony Meyers"
+              src="./src/images/anthony.jpg"
+            />
+            <span class="team__article__imgholder__name">Anthony Meyers</span>
+          </div>
+          <div class="team__article__description">
+            <h3 class="team__article__description__job">
+              Front end development
+            </h3>
+            <p class="team__article__description__text">testing</p>
+          </div>
+        </article>
+      </section>
+    </>
+  );
 }
